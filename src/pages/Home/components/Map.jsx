eec3d07d55f0f6e96165d426539e9865e34dce5a
noahingwersen <noah.ingwersen1@gmail.com>
2023-07-28 17:46:11 +0000

    Updated map

--- src/pages/Home/components/Map.jsx
+++ src/pages/Home/components/Map.jsx
@@ -3,7 +3,7 @@ import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 const Map = ({center, zoom}) => {
   return (
     <div className='w-full h-[93vh]'>
-    <MapContainer center={center} zoom={zoom} scrollWheelZoom={false} >
+    <MapContainer center={center} zoom={zoom} scrollWheelZoom={true} >
         <TileLayer
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
