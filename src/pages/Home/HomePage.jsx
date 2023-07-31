80555f46b93b9714bad85b4b55ad61a3305afccd
noahingwersen <noah.ingwersen1@gmail.com>
2023-07-31 02:17:34 +0000

    Updated loading

--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -2,13 +2,16 @@ import Map from "./components/Map"
 import LoadingSpinner from "../../components/LoadingSpinner"
 import { ErrorBoundary } from "react-error-boundary"
 import MapError from "./components/MapError"
+import { Suspense } from "react"
 
 const HomePage = () => {
     
   return (
     <>
     <ErrorBoundary fallback={<MapError />}>
-      <Map />
+      <Suspense fallback={<LoadingSpinner />}>
+        <Map />  
+      </Suspense>
     </ErrorBoundary>
     </>
   )
--- src/pages/Home/components/Map.jsx
+++ src/pages/Home/components/Map.jsx
@@ -14,11 +14,9 @@ const Map = () => {
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Suspense fallback={<LoadingSpinner />}>
-                {data.map((marker, index) => {
-                    return <LocationMarker key={index} data={marker} />
-                })}
-        </Suspense>
+        {data.map((marker, index) => {
+            return <LocationMarker key={index} data={marker} />
+        })}
     </MapContainer>
     </div>
     )
