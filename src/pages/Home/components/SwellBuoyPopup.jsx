da37f5fd1d9abe7f44557afa6946381485c3a5c9
noahingwersen <noah.ingwersen1@gmail.com>
2023-08-01 22:33:00 +0000

    Updated swell buoy

--- src/pages/Home/components/SwellBuoyPopup.jsx
+++ src/pages/Home/components/SwellBuoyPopup.jsx
@@ -4,7 +4,7 @@ import { useEffect } from 'react'
 
 const SwellBuoyPopup = ({ buoy }) => {
   const meters_to_feet = 3.28084
-  const [data, loading, error] = useApiData(`/api/buoys/swell/${buoy.id}/data`)
+  const [data, loading, error] = useApiData(`/api/buoys/swell/${buoy.id}/data/`)
 
   useEffect(() => {
     if (error) {
@@ -14,44 +14,46 @@ const SwellBuoyPopup = ({ buoy }) => {
 
   return (
     <Popup>
-      {loading ? (
-        <p>Loading...</p>
-      ) : (
-        data && (
-          <div>
-            <strong style={{ fontSize: '20px' }}>{buoy.name}</strong>
-            <br />
-            <b>Buoy ID:</b> {buoy.buoy_id}
-            <br />
-            {data.date}
-            <br />
-            <br />
-            <b>Wave Height:</b> {(data.wave_height * meters_to_feet).toFixed(2)}{' '}
-            ft
-            <br />
-            <b>Average Period:</b> {parseFloat(data.average_period).toFixed(2)}{' '}
-            sec
-            <br />
-            <br />
-            <b>Swell Height:</b>{' '}
-            {(data.swell_height * meters_to_feet).toFixed(2)} ft
-            <br />
-            <b>Swell Period:</b> {parseFloat(data.swell_avg_period).toFixed(2)}{' '}
-            sec
-            <br />
-            <b>Swell Direction:</b>{' '}
-            {parseFloat(data.swell_direction).toFixed(2)} deg
-            <br />
-            <b>Sea Height:</b> {(data.sea_height * meters_to_feet).toFixed(2)}{' '}
-            ft
-            <br />
-            <b>Sea Period:</b> {parseFloat(data.sea_avg_period).toFixed(2)} sec
-            <br />
-            <b>Sea Direction:</b> {parseFloat(data.sea_direction).toFixed(2)}{' '}
-            deg
-          </div>
-        )
-      )}
+      <div>
+        <h3 className='text-lg font-bold'>{buoy.name}</h3>
+        {loading ? (
+          <p>Loading data...</p>
+        ) : (
+          data && (
+            <>
+              <b>Buoy ID:</b> {buoy.buoy_id}
+              <br />
+              {data.date}
+              <br />
+              <br />
+              <b>Wave Height:</b>{' '}
+              {(data.wave_height * meters_to_feet).toFixed(2)} ft
+              <br />
+              <b>Average Period:</b>{' '}
+              {parseFloat(data.average_period).toFixed(2)} sec
+              <br />
+              <br />
+              <b>Swell Height:</b>{' '}
+              {(data.swell_height * meters_to_feet).toFixed(2)} ft
+              <br />
+              <b>Swell Period:</b>{' '}
+              {parseFloat(data.swell_avg_period).toFixed(2)} sec
+              <br />
+              <b>Swell Direction:</b>{' '}
+              {parseFloat(data.swell_direction).toFixed(2)} deg
+              <br />
+              <b>Sea Height:</b> {(data.sea_height * meters_to_feet).toFixed(2)}{' '}
+              ft
+              <br />
+              <b>Sea Period:</b> {parseFloat(data.sea_avg_period).toFixed(2)}{' '}
+              sec
+              <br />
+              <b>Sea Direction:</b> {parseFloat(data.sea_direction).toFixed(2)}{' '}
+              deg
+            </>
+          )
+        )}
+      </div>
     </Popup>
   )
 }
