937da65a58daefe3e552cbc4743786ae3bf0a1eb
noahingwersen <noah.ingwersen1@gmail.com>
2023-08-02 02:11:42 +0000

    Updated loading spinner

--- src/components/LoadingSpinner.jsx
+++ src/components/LoadingSpinner.jsx
@@ -2,7 +2,7 @@ import { MoonLoader } from 'react-spinners'
 
 const LoadingSpinner = () => {
   return (
-    <div className='absolute w-full h-full bg-black opacity-50 flex justify-center items-center z-[1001]'>
+    <div className='w-full h-full bg-black opacity-50 flex justify-center items-center z-[1001]'>
       <MoonLoader color='#36d7b7' size={60} />
     </div>
   )
