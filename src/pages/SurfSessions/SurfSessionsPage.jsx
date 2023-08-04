40496dbc2230164e4dd67d05391c7291c045b424
noahingwersen <noah.ingwersen1@gmail.com>
2023-08-04 17:57:05 +0000

    Added 404 route

--- src/pages/SurfSessions/SurfSessionsPage.jsx
+++ src/pages/SurfSessions/SurfSessionsPage.jsx
@@ -1,9 +1,11 @@
 import { Routes, Route } from 'react-router-dom'
 import AddSurfSession from './components/AddSurfSession'
+import NotFound from '../../components/NotFound'
 
 const SurfSessionsPage = () => {
   return (
     <Routes>
+      <Route element={<NotFound />} path='*' />
       <Route element={<AddSurfSession />} path='/add' />
     </Routes>
   )
--- src/pages/SurfSpots/SurfSpotsPage.jsx
+++ src/pages/SurfSpots/SurfSpotsPage.jsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from 'react-router-dom'
 import SurfSpotTable from './components/SurfSpotTable'
 import AddSurfSpot from './components/AddSurfSpot'
+import NotFound from '../../components/NotFound'
 
 const SurfSpotsPage = () => {
   return (
     <Routes>
+      <Route element={<NotFound />} path='*' />
       <Route element={<SurfSpotTable />} path='/' exact />
       <Route element={<AddSurfSpot />} path='/add' />
     </Routes>
